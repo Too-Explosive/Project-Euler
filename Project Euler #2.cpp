2aa1922acb62da2aa6b3c56045906f00aee24ba4
Too-Explosive <mukurkur@gmail.com>
2016-11-17 09:59:57 -0800

    Update Project Euler #2.cpp

--- Project Euler #2.cpp
+++ Project Euler #2.cpp
@@ -1,23 +1,15 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+#include <iostream>
 
-\f0\fs24 \cf0 #include <iostream>\
-\
-int main() \{\
-	int fib1 = 0, fib2 = 1, temp, sum = 0;\
-	while(fib1 < 4000000)\
-	\{\
-	    if(fib1 % 2 == 0)\
-	        sum = sum + fib1;\
-	    temp = fib1 + fib2;\
-	    fib1 = fib2;\
-	    fib2 = temp;\
-	  \
-	\}\
-	std::cout << sum;\
-	return 0;\
-\}\
+int main() {
+	int fib1 = 0, fib2 = 1, temp, sum = 0;
+	while(fib1 < 4000000)
+	{
+	    if(fib1 % 2 == 0)
+	        sum = sum + fib1;
+	    temp = fib1 + fib2;
+	    fib1 = fib2;
+	    fib2 = temp;
+	}
+	std::cout << sum;
+	return 0;
 }
